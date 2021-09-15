62f481af4019cd7c72ce34d5355e8fb501868999
AminaxAce <90779294+AminaxAce@users.noreply.github.com>
2021-09-15 19:19:31 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import image from './images/image2.jpg'
+
+const App=()=> {
+  return (
+    <div className='kan'>
+      <p> Administrator Log In</p>
+      <img className="myPic" src={image} alt="xx2"/>
+    </div>
+
+  )
+}
+
+export default App;
